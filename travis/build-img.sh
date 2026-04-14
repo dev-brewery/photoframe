85d4c321fc5fd0c580cb7bdc375671ed7573843c
Michael Brewer <mb1985@gmail.com>
2026-04-13 21:49:21 -0400

    Point pi-gen references at the dev-brewery fork

--- travis/build-img.sh
+++ travis/build-img.sh
@@ -11,7 +11,7 @@ if [ -d pi-gen ]; then
   sudo rm -rf pi-gen
 fi
 
-git clone https://github.com/mrworf/pi-gen.git
+git clone --branch bookworm-photoframe https://github.com/dev-brewery/pi-gen.git
 cd pi-gen
 echo >config.local "PHOTOFRAME_SRC=\"${PHOTOFRAME_SRC}\""
 sudo ./build.sh
